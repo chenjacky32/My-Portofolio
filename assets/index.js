47773cf19fa29c3a1f3d490e4f754a11bab233d1
Jacky Chen <chenjacky32@gmail.com>
2024-02-07 17:43:19 +0700

    feat:tambah isi bagian project

--- assets/index.js
+++ assets/index.js
@@ -7,7 +7,6 @@ document.querySelector("#hamburger-menu").onclick = () => {
 };
 
 //hilangkan nav
-
 const hamburger = document.querySelector("#hamburger-menu");
 
 document.addEventListener("click", function (e) {
@@ -16,5 +15,9 @@ document.addEventListener("click", function (e) {
   }
 });
 
+//scroll reveal
+ScrollReveal({ reset: true, distance: "50px", duration: 2000, delay: 300 });
 
-
+ScrollReveal().reveal(".hero-content,h4", { origin: "top" });
+ScrollReveal().reveal("#gambar1,.about-me-pict,.about-content,.map", { origin: "bottom" });
+ScrollReveal().reveal(".projects-pict", { origin: "left" });
